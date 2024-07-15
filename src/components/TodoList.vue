848b38162733a4f552f2d552baacdad1035d2aeb
Lesnier Garcia <lesnier.estrada@gmail.com>
2024-07-15 15:55:56 -0400

    feat: Add localStorage and Sorting Todo List

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,15 @@
 <script lang="ts" setup>
 import type { Ref } from 'vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import ListItem from './ListItem.vue';
 
+// Definir el tipo de objeto de los elementos de la lista
 type Item = {
   title: string
   checked?: boolean
 }
 
+// Crear una referencia reactiva a la lista de elementos
 const listItems: Ref<Item[]> = ref([
   { title: 'Make a todo list app', checked: true },
   { title: 'Predict the weather', checked: false },
@@ -20,6 +22,10 @@ const listItems: Ref<Item[]> = ref([
   { title: 'Publish my work' }
 ])
 
+// Crear una lista ordenada basada en el estado actual de los elementos de la lista
+const sortedList = computed(() => [...listItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0)));
+
+// Actualizar el estado de un elemento de la lista
 const updateItem = (item: Item): void => {
   const updatedItem = findItemInList(item)
   if (updatedItem) {
@@ -27,10 +33,26 @@ const updateItem = (item: Item): void => {
   }
 }
 
+// Guardar la lista en el almacenamiento local
+const setToLocalStorage = (items: Item[]): void => {
+  localStorage.setItem('list-items', JSON.stringify(items));
+}
+
+// Obtener la lista del almacenamiento local
+const getFromLocalStorage = (): Item[] | [] => {
+  const stored = localStorage.getItem('list-items');
+  if (stored) {
+    return JSON.parse(stored);
+  }
+  return [];
+}
+
+// Encontrar un elemento en la lista basado en el título
 const findItemInList = (item: Item): Item | undefined => {
   return listItems.value.find((itemInList: Item) => itemInList.title === item.title)
 }
 
+// Cambiar el estado "checked" de un elemento
 const toggleItemChecked = (item: Item): void => {
   item.checked = !item.checked
 }
@@ -38,10 +60,8 @@ const toggleItemChecked = (item: Item): void => {
 
 <template>
   <ul>
-    <li :key="key" v-for="(item, key) in listItems">
-      <ListItem :is-checked="item.checked" @click.prevent.self="updateItem(item)">{{
-        item.title
-      }}</ListItem>
+    <li :key="key" v-for="(item, key) in sortedList">
+      <ListItem :is-checked="item.checked" @click.prevent.self="updateItem(item)">{{ item.title }}</ListItem>
     </li>
   </ul>
 </template>
